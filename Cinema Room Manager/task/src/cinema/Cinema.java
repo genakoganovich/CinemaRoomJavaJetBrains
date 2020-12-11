43d35e256a9fd064cbca80f59ade3916affda8fd
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-11 12:55:25 +0200

    0001 stage 1/5 done

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,8 +1,25 @@
 package cinema;
 
 public class Cinema {
-
+    public static final int ROWS = 7;
+    public static final int SEATS = 8;
     public static void main(String[] args) {
-        // Write your code here
+        //==================================
+        // printing the scheme
+        //==================================
+        System.out.println("Cinema:");
+        System.out.print(" ");
+        for (int i = 1; i <= SEATS; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 1; i <= ROWS; i++) {
+            System.out.print(i);
+            for (int j = 1; j <= SEATS; j++) {
+                System.out.print(" " + "S");
+            }
+            System.out.println();
+        }
+
     }
 }
