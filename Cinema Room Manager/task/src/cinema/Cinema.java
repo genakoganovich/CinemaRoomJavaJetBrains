f6f00412f603402f9526630c4a46ee4cbbbf0eb7
Gennadiy Koganovich <genakoganovich@gmail.com>
2020-12-11 13:45:06 +0200

    0002 stage 1/5 done

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,25 +1,53 @@
 package cinema;
+import java.util.Scanner;
 
 public class Cinema {
-    public static final int ROWS = 7;
-    public static final int SEATS = 8;
+    public static int rows = 7;
+    public static int seats = 8;
+    public static final int MAX_SEATS = 60;
+    public static final int SMALL_ROOM_PRICE = 10;
+    public static final int LARGE_ROOM_FRONT_PRICE = 10;
+    public static final int LARGE_ROOM_BACK_PRICE = 8;
+
     public static void main(String[] args) {
         //==================================
         // printing the scheme
         //==================================
-        System.out.println("Cinema:");
-        System.out.print(" ");
-        for (int i = 1; i <= SEATS; i++) {
-            System.out.print(" " + i);
-        }
-        System.out.println();
-        for (int i = 1; i <= ROWS; i++) {
-            System.out.print(i);
-            for (int j = 1; j <= SEATS; j++) {
-                System.out.print(" " + "S");
-            }
-            System.out.println();
+//        System.out.println("Cinema:");
+//        System.out.print(" ");
+//        for (int i = 1; i <= seats; i++) {
+//            System.out.print(" " + i);
+//        }
+//        System.out.println();
+//        for (int i = 1; i <= rows; i++) {
+//            System.out.print(i);
+//            for (int j = 1; j <= seats; j++) {
+//                System.out.print(" " + "S");
+//            }
+//            System.out.println();
+//        }
+
+        // initialize variables commit 2
+        int income = 0;
+        Scanner scanner = new Scanner(System.in);
+
+        // get number of rows and seats from a user
+        System.out.println("Enter the number of rows:");
+        rows = scanner.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        seats = scanner.nextInt();
+
+        // calculate total income
+        if (rows * seats <= MAX_SEATS) {
+            income = rows * seats * SMALL_ROOM_PRICE;
+        } else {
+            int frontRows = rows / 2;
+            int backRows = rows - frontRows;
+            income = (frontRows * LARGE_ROOM_FRONT_PRICE + backRows * LARGE_ROOM_BACK_PRICE) * seats;
         }
 
+        // print result
+        System.out.println("Total income:");
+        System.out.println("$" + income);
     }
 }
